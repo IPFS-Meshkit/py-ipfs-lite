a5fe42bf3d722f3ff636ecfa4cf1f114a600753b
sumanjeet0012 <sumanjeet0012@gmail.com>
2026-07-25 01:07:00 +0530

    chore(bootstrap): restore default bootstrap nodes

--- py_ipfs_lite/cli.py
+++ py_ipfs_lite/cli.py
@@ -55,7 +55,10 @@ def _get_key_pair(seed: str | None, config: Config | None = None) -> Any:
 
 
 DEFAULT_BOOTSTRAP_PEERS = [
-    "/ip4/52.7.183.75/udp/4001/quic-v1/p2p/12D3KooWS9GDjG635iEMB2Zz3iSXpxgPhNNpmDCuo4ynSkitoyut",
+    "/dnsaddr/bootstrap.libp2p.io/p2p/QmNnooDu7bfjPFoTZYxMNLWUQJyrVwtbZg5gBMjTezGAJN",
+    "/dnsaddr/bootstrap.libp2p.io/p2p/QmQCU2EcMqAqQPR2i9bChDtGNJchTbq5TbXJJ16u19uLTa",
+    "/dnsaddr/bootstrap.libp2p.io/p2p/QmbLHAnMoJPWSCR5Zhtx6BHJX9KiKNN6tpvbUcqanj75Nb",
+    "/ip4/104.131.131.82/tcp/4001/p2p/QmaCpDMGvV2BGHeYERUEnRQAwe3N8SzbUtfsmvsqQLuvuJ",
 ]
 
 from contextlib import asynccontextmanager
